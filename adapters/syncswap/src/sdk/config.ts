2bc03c514f1f74b9e660e5b6db30b4416840192d
Amedrontadora <steve@syncswap.xyz>
2024-04-24 08:13:46 +0800

    chore: comment subgraph url

--- adapters/syncswap/src/sdk/config.ts
+++ adapters/syncswap/src/sdk/config.ts
@@ -3,5 +3,5 @@ export const enum CHAINS{
 }
 
 export const SUBGRAPH_URLS = {
-    [CHAINS.LINEA]: "https://gateway-arbitrum.network.thegraph.com/api/ce0ba3625ebbbd3c4b5a2af394dc8e47/subgraphs/id/3xpZFx5YNWzqemwdtRhyaTXVidKNnjY19XAWoHtvR6Lh"
+    [CHAINS.LINEA]: "https://gateway-arbitrum.network.thegraph.com/api/ce0ba3625ebbbd3c4b5a2af394dc8e47/subgraphs/id/3xpZFx5YNWzqemwdtRhyaTXVidKNnjY19XAWoHtvR6Lh" // linea network
 }
